beed0a7c51da51d5acba524a4c7a2386394e9db4
Anas Kaarov <anasbinkurban@gmail.com>
2022-09-11 21:56:22 +0600

    11/09/22

---
+++ easy/20. Valid Parentheses.py
@@ -0,0 +1,27 @@
+# s = "([)]"
+s = "){"
+
+
+class Solution:
+    def isValid(self, s):
+        result = []
+        check = [')', '}', ']']
+        if len(s) <= 1:
+            return False
+        while s:
+            if s[0] in check:
+                if result:
+                    if result[-1] == '(' and s[0] == check[0] or result[-1] == '{' and s[0] == check[1] or \
+                            result[-1] == '[' and s[0] == check[2]:
+                        result.pop()
+                else:
+                    return False
+            elif s[0] in ['(', '{', '[']:
+                result.append(s[0])
+            s = s[1:]
+        if not result:
+            return True
+        else:
+            return False
+
+print(Solution().isValid(s))
