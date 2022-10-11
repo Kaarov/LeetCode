dc8f69f7670d41acaf1c2887ac36f8ee8f786c87
Anas Kaarov <anasbinkurban@gmail.com>
2022-10-11 12:34:42 +0600

    11/10/22

---
+++ easy/724. Find Pivot Index.py
@@ -0,0 +1,17 @@
+numbers = [1, 7, 3, 6, 5, 6]
+# numbers = [2, 1, -1]
+
+
+class Solution(object):
+    def pivotIndex(self, nums):
+        left, right = 0, sum(nums)
+        for i in range(len(nums)):
+            right -= nums[i]
+            if left == right:
+                return i
+            left += nums[i]
+        return -1
+
+
+test = Solution()
+print(test.pivotIndex(numbers))
