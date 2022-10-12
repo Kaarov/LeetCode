f561a806c34aa707c90843ed786b27cad49cc481
Anas Kaarov <anasbinkurban@gmail.com>
2022-10-12 19:56:59 +0600

    12/10/22

--- easy/976. Largest Perimeter Triangle.py
+++ easy/976. Largest Perimeter Triangle.py
@@ -3,8 +3,16 @@ numbers = [2, 1, 2]
 
 class Solution:
     def largestPerimeter(self, nums):
-        pass
+        nums.sort(reverse=True)
+
+        for i in range(3, len(nums) + 1):
+            if nums[i - 3] < nums[i - 2] + nums[i - 1]:
+                return sum(nums[i - 3:i])
+
+        return 0
 
 
 test = Solution()
 print(test.largestPerimeter(numbers))
+
+# Done ✅
