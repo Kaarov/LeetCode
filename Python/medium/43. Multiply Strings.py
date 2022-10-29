a990a9c34e0957eb6670ce558a19d4994a56b71d
Anas Kaarov <anasbinkurban@gmail.com>
2022-10-29 12:25:05 +0600

    29/10/22

---
+++ Python/medium/43. Multiply Strings.py
@@ -0,0 +1,13 @@
+number1 = "2"
+number2 = "3"
+
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1) * int(num2))
+
+
+slt = Solution()
+print(slt.multiply(number1, number2))
+
+# Done ✅
