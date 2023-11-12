d8a05c2ffd261eacb345da5e4e5e2433cfcdc3b2
Anas Kaarov <anasbinkurban@gmail.com>
2023-11-12 20:17:49 +0600

    feat: Linked List

---
+++ Data Structures/Linked List.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node()
+
+    def append(self, data):
+        new_node = Node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+
+    def length(self):
+        cur = self.head
+        total = 0
+        while cur.next != None:
+            total += 1
+            cur = cur.next
+        return total
+
+    def display(self):
+        elements = []
+        cur_node = self.head
+        while cur_node.next != None:
+            cur_node = cur_node.next
+            elements.append(cur_node.data)
+        return elements
+
+    def get(self, index):
+        if index >= self.length():
+            return "ERROR: 'GET' Index out of range"
+        cur_index = 0
+        cur_node = self.head
+        while True:
+            cur_node = cur_node.next
+            if cur_index == index:
+                return cur_node.data
+            cur_index += 1
+
+    def erase(self, index):
+        if index >= self.length():
+            return "ERROR: 'GET' Index out of range"
+        cur_index = 0
+        cur_node = self.head
+        while True:
+            last_node = cur_node
+            cur_node = cur_node.next
+            if cur_index == index:
+                last_node.next = cur_node.next
+                return index
+            cur_index += 1
