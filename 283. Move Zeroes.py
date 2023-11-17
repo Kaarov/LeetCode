0f6ead3c8db38381bd785ef2a9a16eec95f9e3a4
Anas Kaarov <anasbinkurban@gmail.com>
2023-11-17 20:32:07 +0600

    feat: (Easy) 283. Move Zeroes

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,16 @@
+from typing import List
+
+nums = [0, 0, 1]
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        count = nums.count(0)
+        i = 0
+        while count != 0:
+            if nums[i] == 0:
+                count -= 1
+                for j in range(i, len(nums) - 1):
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+            else:
+                i += 1
