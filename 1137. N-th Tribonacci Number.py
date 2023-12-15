fccdd6d57248656559dc64ce1e3b6389296365dc
Anas Kaarov <anasbinkurban@gmail.com>
2023-12-15 22:08:50 +0600

    feat: (Easy) 1137. N-th Tribonacci Number

---
+++ 1137. N-th Tribonacci Number.py
@@ -0,0 +1,14 @@
+n = 4
+
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        array = [0, 1, 1]
+        for i in range(3, n):
+            array.append(sum(array))
+            array.remove(array[0])
+        return sum(array) if n > 2 else array[n]
+
+
+slt = Solution()
+print(slt.tribonacci(n))
