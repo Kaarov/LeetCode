4f3bccfc9f75d813897e6416e299d4b0f05cce81
Anas Kaarov <anasbinkurban@gmail.com>
2024-11-18 21:28:58 +0600

    feat: (Easy) 682. Baseball Game

---
+++ 682. Baseball Game.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        ans = []
+        for operation in operations:
+            if operation == 'C':
+                ans.pop()
+            elif operation == 'D':
+                ans.append(ans[-1] * 2)
+            elif operation == '+':
+                ans.append(ans[-1] + ans[-2])
+            else:
+                ans.append(int(operation))
+        return sum(ans)
+
+
+ops = ["5", "2", "C", "D", "+"]
+slt = Solution()
+print(slt.calPoints(ops))
