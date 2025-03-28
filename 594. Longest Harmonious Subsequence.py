3ac16bb68ff7b764e8cac5082bc5b7c7fb8fd58d
Anas Kaarov <anasbinkurban@gmail.com>
2025-03-28 11:11:32 +0600

    feat: [Easy] 594. Longest Harmonious Subsequence

---
+++ 594. Longest Harmonious Subsequence.py
@@ -0,0 +1,26 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        c = Counter(nums)
+        num_set = c.keys()
+        ans = 0
+        for key in num_set:
+            if key - 1 in num_set:
+                maximum = max(ans, c[key] + c[key - 1])
+                ans = maximum
+            if key + 1 in num_set:
+                maximum = max(ans, c[key] + c[key + 1])
+                ans = maximum
+        return ans
+
+
+if __name__ == '__main__':
+    slt = Solution()
+    print(slt.findLHS([1, 3, 2, 2, 5, 2, 3, 7]))  # 5
+    print(slt.findLHS([1, 2, 3, 4]))  # 2
+    print(slt.findLHS([1, 1, 1, 1]))  # 0
+
+# Done ✅
