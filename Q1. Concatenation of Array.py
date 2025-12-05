b83691c4f7354d259496d6c69da3d6407b9cc6ad
Kaarov <anasbinkurban@gmail.com>
2025-12-05 18:58:58 +0600

    refactor: [Easy] Q1. Concatenation of Array

--- Q1. Concatenation of Array.py
+++ Q1. Concatenation of Array.py
@@ -3,7 +3,7 @@ class Solution:
         return nums * 2
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     slt = Solution()
     slt.getConcatenation(nums=[1, 2, 1])  # [1, 2, 1, 1, 2, 1]
     slt.getConcatenation(nums=[1, 3, 2, 1])  # [1, 3, 2, 1, 1, 3, 2, 1]
