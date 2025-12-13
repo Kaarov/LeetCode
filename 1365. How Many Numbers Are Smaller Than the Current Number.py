d361b6ff3f74bc885e3ca627c53e3799d2af69d8
Kaarov <anasbinkurban@gmail.com>
2025-12-13 19:37:22 +0600

    refactor: [Easy] 1365. How Many Numbers Are Smaller Than the Current Number

--- 1365. How Many Numbers Are Smaller Than the Current Number.py
+++ 1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# Attempt 1
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         ans = []
@@ -13,10 +14,27 @@ class Solution:
         return ans
 
 
+# Attempt 2
+class Solution2:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        numbers = {}
+
+        for i, num in enumerate(sorted(nums)):
+            if num not in numbers:
+                numbers[num] = i
+
+        return [numbers[num] for num in nums]
+
+
 if __name__ == '__main__':
     slt = Solution()
-    print(slt.smallerNumbersThanCurrent([8, 1, 2, 2, 3]))  # [4, 0, 1, 1, 3]
-    print(slt.smallerNumbersThanCurrent([6, 5, 4, 8]))  # [2, 1, 0, 3]
-    print(slt.smallerNumbersThanCurrent([7, 7, 7, 7]))  # [0, 0, 0, 0]
+    assert slt.smallerNumbersThanCurrent([8, 1, 2, 2, 3]) == [4, 0, 1, 1, 3]
+    assert slt.smallerNumbersThanCurrent([6, 5, 4, 8]) == [2, 1, 0, 3]
+    assert slt.smallerNumbersThanCurrent([7, 7, 7, 7]) == [0, 0, 0, 0]
+
+    slt2 = Solution2()
+    assert slt2.smallerNumbersThanCurrent([8, 1, 2, 2, 3]) == [4, 0, 1, 1, 3]
+    assert slt2.smallerNumbersThanCurrent([6, 5, 4, 8]) == [2, 1, 0, 3]
+    assert slt2.smallerNumbersThanCurrent([7, 7, 7, 7]) == [0, 0, 0, 0]
 
 # Done ✅
