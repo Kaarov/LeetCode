e4d4e2416bb00d398bcf18fa0b54aef8e660fda2
Kaarov <anasbinkurban@gmail.com>
2026-01-21 19:45:06 +0600

    refactor: [Easy] 1732. Find the Highest Altitude

--- 1732. Find the Highest Altitude.py
+++ 1732. Find the Highest Altitude.py
@@ -1,17 +1,17 @@
-from typing import List
-
-gain = [-5, 1, 5, 0, -7]
-
-
 class Solution:
-    def largestAltitude(self, gain: List[int]) -> int:
-        ans = [0, ]
-        for i in range(len(gain)):
-            ans += [ans[i] + gain[i]]
-        return max(ans)
+    def largestAltitude(self, gain: list[int]) -> int:
+        ans = 0
+        sum = 0
+        for i in gain:
+            sum += i
+            ans = max(ans, sum)
+
+        return ans
 
 
-slt = Solution()
-print(slt.largestAltitude(gain))
+if __name__ == "__main__":
+    slt = Solution()
+    assert slt.largestAltitude([-5, 1, 5, 0, -7]) == 1
+    assert slt.largestAltitude([-4, -3, -2, -1, 4, 3, 2]) == 0
 
 # Done ✅
