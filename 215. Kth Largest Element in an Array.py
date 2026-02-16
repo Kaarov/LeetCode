8b68f6847f9105ac8c9aae4b04be12ecfaa0954f
Kaarov <anasbinkurban@gmail.com>
2026-02-16 23:00:29 +0600

    refactor: [Medium] 215. Kth Largest Element in an Array

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -1,13 +1,22 @@
-from typing import List
+from random import choice
 
 
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    # version 1
+    def findKthLargest(self, nums: list[int], k: int) -> int:
         nums.sort(reverse=True)
         return nums[k - 1]
 
+    # version 2
+    # def findKthLargest(self, nums: list[int], k: int) -> int:
+    #     p, l, m, r = choice(nums), [], [], []
+    #     for v in nums: (m, l, r)[(p < v) - (v < p)].append(v)
+    #     if k <= len(l): return self.findKthLargest(l, k)
+    #     if len(l) + len(m) < k: return self.findKthLargest(r, k - len(l) - len(m))
+    #     return p
 
-nums = [3, 2, 1, 5, 6, 4]
-k = 2
-slt = Solution()
-print(slt.findKthLargest(nums, k))
+
+if __name__ == "__main__":
+    slt = Solution()
+    assert slt.findKthLargest([3, 2, 1, 5, 6, 4], 2) == 5
+    assert slt.findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4) == 4
